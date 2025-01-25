9293856bd2df995dc604e41e40769630e195617f
knonode <algohmp@gmail.com>
2025-01-25 21:03:16 +0100

    reverted to localhost working version

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,20 @@
 import adapter from '@sveltejs/adapter-netlify';
+import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess({
+		scss: {
+			prependData: `@use 'sass:math';
+										@import './src/styles/mixins';
+										@import './src/styles/variables/breakpoints';`
+		}
+	}),
+
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+	},
+
+
 };
 
 export default config;
