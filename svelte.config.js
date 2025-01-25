3ef5a829dbbe12528ac635fb82a0902b320f22dc
knonode <algohmp@gmail.com>
2025-01-25 19:11:06 +0100

    Updated Netlify configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,10 @@
 import adapter from '@sveltejs/adapter-netlify';
-import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess({
-		scss: {
-			prependData: `@use 'sass:math';
-										@import './src/styles/mixins';
-										@import './src/styles/variables/breakpoints';`
-		}
-	}),
-
 	kit: {
 		adapter: adapter()
-	},
-
-
+	}
 };
 
 export default config;
